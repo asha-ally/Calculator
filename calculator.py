cde15225aa3c16dc217bb2a4224e1bbd7ca36d05
asha-ally <asherally079@gmail.com>
2019-04-12 18:12:59 +0300

    calculator assignment

---
+++ calculator.py
@@ -0,0 +1,31 @@
+def add(a,b):
+	answer=a+b
+	return answer
+	
+def subtract(a,b):
+	answer=a-b
+	return answer
+
+def multiply(a,b):
+	answer=a*b
+	return answer
+
+def divide(a,b):
+	answer=a/b
+	return answer
+
+def modulus(a,b):
+	answer=a%b
+	return answer
+
+def whole_number(a,b):
+	answer=a//b
+	return answer
+
+def exponenet(1,m):
+	answer=j//k
+	return answer
+
+def percentage(n,o)
+answer=n%o
+return answer
